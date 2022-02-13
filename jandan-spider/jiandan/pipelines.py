fa94b6e59bb4163fc4540baa623ea7317f3efa59
fengyuan2333 <86827439+fengyuan2333@users.noreply.github.com>
2022-02-13 17:18:44 +0800

    Update pipelines.py

--- jandan-spider/jiandan/pipelines.py
+++ jandan-spider/jiandan/pipelines.py
@@ -15,24 +15,6 @@ class JiandanPipeline:
         return item
 
 class ImagesPathPipeline(ImagesPipeline):
-    # def get_media_requests(self, item, info):
-    #     # 这个方法是在发送下载请求之前调用的，其实这个方法本身就是去发送下载请求的
-    #     request_objs=super(Images699Pipeline, self).get_media_requests(item,info)
-    #     for request_obj in request_objs:
-    #         request_obj.item=item
-    #     return request_objs
-
-    # def file_path(self, request, response=None, info=None):
-    #     # 这个方法是在图片将要被存储的时候调用，来获取这个图片存储的路径
-    #     path=super(Images699Pipeline, self).file_path(request,response,info)
-    #     category=request.item.get('category')
-    #     image_store=settings.IMAGES_STORE
-    #     category_path=os.path.join(image_store,category)
-    #     if not os.path.exists(category_path):
-    #         os.makedirs(category_path)
-    #     image_name=path.replace("full/","")
-    #     image_path=os.path.join(category_path,image_name)
-    #     return image_path
 
     def file_path(self, request, response=None, info=None, *, item=None):
 
